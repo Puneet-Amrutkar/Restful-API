66f6f45e5eb21039c85c14d9114ab2b399fa3dfb
Puneet-Amrutkar <puneet.tech15@gmail.com>
2023-10-16 22:00:30 +0530

    completed CRUD operations

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/products", {useNewUrlParser:true, us
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.json);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
 
 const productSchema = new mongoose.Schema({
     name:String,
@@ -21,26 +21,90 @@ const productSchema = new mongoose.Schema({
 
 const Product = new mongoose.model("Product", productSchema);
 
-// API to create the product
-app.post("api/v1/newproduct", async (req, res) => {
-    const product = await Product.create(req.body);
+// API to create new product
+app.post("/api/v1/newproduct", async (req, res) => {
+    try 
+    {
+        const product = await Product.create(req.body);
 
-    res.status(201).json({
-        success:true,
-        product
-    })
+        res.status(201).json({
+            success:true,
+            product
+        })
+    }
+    catch(e)
+    {
+        console.log("error occured!");
+        console.log(e);
+    }
 })
 
 // API to get the products
-app.get("api/v1/products", async (req, res) => {
-    const products = await Product.find();
+app.get("/api/v1/products", async (req, res) => {
+    try
+    {
+        const products = await Product.find();
 
-    res.status(200).json({
-        success:true,
-        products
-    })
+        res.status(200).json({
+            success:true,
+            products
+        })
+    }
+    catch(e)
+    {
+        console.log("error occured!");
+        console.log(e);
+    }
+})
+    
+
+// API to update product
+app.put("/api/v1/product/:id", async (req, res) => {
+    try
+    {
+        let product = await Product.findById(req.params.id);
+
+        product = await Product.findByIdAndUpdate(req.params.id,req.body, {new:true, useFindAndModify:true, runValidators:true})
+        res.status(200).json({
+            success:true,
+            product
+        })
+    }
+    catch(e)
+    {
+        console.log("error occured!");
+        console.log(e);
+    }
+})
+
+// API to delete product
+app.delete("/api/v1/product/:id", async (req, res) => {
+    try
+    {
+        const product = await Product.findById(req.params.id);
+        
+        if(!product)
+        {
+            return res.status(500).json({
+                success:false,
+                message:"product not found",
+            })
+        }
+
+        await product.remove();
+
+        res.status(200).json({
+            success:true,
+            message:"product deleted successfully",
+        })
+    }
+    catch(e)
+    {
+        console.log("error occured!");
+        console.log(e);
+    }
 })
 
 app.listen(5001, () => {
     console.log('server is working at port 5001');
-})
+});
